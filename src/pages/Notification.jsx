80a88345dfda06093eca2ba14c29602b6d0d8dcb
dev-prathamesh <prathamesh.c@skysoft.co.in>
2024-04-04 22:50:47 +0530

    complete with the notification page

--- src/pages/Notification.jsx
+++ src/pages/Notification.jsx
@@ -1,44 +1,45 @@
 import Layout from "../components/Layout";
-import { Box, TextField } from "@mui/material";
+import { Box, TextField, Typography } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import BgImage from "../assets/icon/message_bg.jpg";
 
 const Notification = () => {
   return (
-    <Layout isBack title={"Notification"}>
-      <Box
-        height={"calc(100vh - 9.5rem)"}
-        position={"relative"}
-        marginBottom={"4.5rem"}
-        display={"flex"}
-        justifyContent={"center"}
-        p={"8px"}
-      >
-        <Box
-          maxWidth={520}
-          width={"100%"}
-          height={"100%"}
-          boxShadow={
-            "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px"
-          }
-          borderRadius={2}
-          p={"8px 0"}
-          flexDirection={"column"}
-          display={"flex"}
-          justifyContent={"end"}
-          position={"relative"}
-        >
+    <Layout>
+      <Box className="notification-main">
+        <Box className="notification-message-box">
           <Box
             height={"100%"}
             m={"0 8px 8px 8px"}
             overflow={"auto"}
+            flexDirection={"column"}
+            gap={"40px"}
+            display={"flex"}
+            justifyContent={"right"}
           >
-            {[
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0,
-            ].map((item, i) => (
-              <Box key={i}>
-                <h1>ajkhsdlkd</h1>
+            {[0, 1, 0, 1].map((item, i) => (
+              <Box
+                key={i}
+                display={"flex"}
+                justifyContent={"right"}
+                flexDirection={"column"}
+                alignItems={item == 0 ? "end" : "start"}
+              >
+                <Typography fontSize={"10px"}>
+                  09:30 pm 04-April-2024
+                </Typography>
+                <Box
+                  width={"80%"}
+                  bgcolor={"lightblue"}
+                  p={"10px"}
+                  borderRadius={"10px"}
+                >
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Itaque illo voluptate est? Asperiores enim illo modi
+                    suscipit nobis eos fugit amet excepturi facilis facere.
+                  </Typography>
+                </Box>
               </Box>
             ))}
           </Box>
