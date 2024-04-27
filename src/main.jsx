a6aab922a26ffb0c90f0b163ee21d68875047bae
dev-prathamesh <prathamesh.c@skysoft.co.in>
2024-04-27 23:45:42 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import "react-toastify/dist/ReactToastify.min.css";
+import { injectStyle } from "react-toastify/dist/inject-style";
+injectStyle();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
