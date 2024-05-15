a70c4737c6081d76fbffdb0ddc64be9cc24caa4e
dev-prathamesh <prathamesh.c@skysoft.co.in>
2024-05-15 23:32:25 +0530

    done

--- src/api-services/authApiService.js
+++ src/api-services/authApiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const authApiService = () => {
-  const BASE_URL = "https://zany-jade-sockeye-suit.cyclic.app";
+  const BASE_URL = "https://class-compass-server.onrender.com";
   // const BASE_URL = "http://localhost:8000";
   const login = async (user) => {
     return await axios.post(`${BASE_URL}/auth/login`, user);
