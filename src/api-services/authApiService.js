191d172afa7a4aa36a2b0a25c7d81a486d9b2036
dev-prathamesh <prathamesh.c@skysoft.co.in>
2024-05-26 23:20:52 +0530

    done

--- src/api-services/authApiService.js
+++ src/api-services/authApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const authApiService = () => {
-  const BASE_URL = "https://class-compass-server.onrender.com";
-  // const BASE_URL = "http://localhost:8000";
+  // const BASE_URL = "https://class-compass-server.onrender.com";
+  const BASE_URL = "http://localhost:8000";
   const login = async (user) => {
     return await axios.post(`${BASE_URL}/auth/login`, user);
   };
@@ -21,11 +21,16 @@ export const authApiService = () => {
   const changePassword = async (userData) => {
     return await axios.post(`${BASE_URL}/auth/change-password`, userData);
   };
+
+  const whitelistUser = async (userData) => {
+    return await axios.post(`${BASE_URL}/auth/add-whitelist`, userData);
+  };
   return {
     login,
     register,
     forgotPassword,
     verifyOtp,
     changePassword,
+    whitelistUser,
   };
 };
